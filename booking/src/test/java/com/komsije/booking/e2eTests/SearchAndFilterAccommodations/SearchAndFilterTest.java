99f43622e47a620d3585d0250c2d03bf25a8fafb
aleksaqm <perovic.aleksa03@gmail.com>
2024-01-22 13:40:50 +0100

    feat: added base for selenium e2e tests

---
+++ booking/src/test/java/com/komsije/booking/e2eTests/SearchAndFilterAccommodations/SearchAndFilterTest.java
@@ -0,0 +1,5 @@
+package com.komsije.booking.e2eTests.SearchAndFilterAccommodations;
+
+public class SearchAndFilterTest {
+
+}
---
+++ booking/src/test/java/com/komsije/booking/e2eTests/SearchAndFilterAccommodations/TestBase.java
@@ -0,0 +1,27 @@
+package com.komsije.booking.e2eTests.SearchAndFilterAccommodations;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestBase {
+    public static WebDriver driver;
+
+    @BeforeAll
+    public static void initializeWebDriver() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+    }
+
+    @AfterAll
+    public static void quitDriver() {
+        driver.quit();
+    }
+}

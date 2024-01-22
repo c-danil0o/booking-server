3650ad4dfa4a187bff5b24eb1c4c3eab65c0dc3c
aleksaqm <perovic.aleksa03@gmail.com>
2024-01-22 14:26:01 +0100

    feat: added isOpen to home page

--- booking/src/test/java/com/komsije/booking/e2eTests/pages/HomePage.java
+++ booking/src/test/java/com/komsije/booking/e2eTests/pages/HomePage.java
@@ -1,4 +1,33 @@
 package com.komsije.booking.e2eTests.pages;
 
+import com.komsije.booking.e2eTests.helper.Helper;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+import java.time.Duration;
+
 public class HomePage {
+    private final WebDriver driver;
+    private final static String PAGE_URL = " http://localhost:4200/";
+
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+        driver.get(PAGE_URL);
+        PageFactory.initElements(this.driver, this);
+    }
+
+    public boolean isOpen(){
+        Duration duration = Duration.ofSeconds(10);
+        boolean isLoaded = (new WebDriverWait(driver, duration))
+                .until(ExpectedConditions.textToBePresentInElementLocated(By.id("title"), "Make your holiday"));
+        Helper.takeScreenshoot(driver, "ucitaoStanicu");
+        return isLoaded;
+    }
+
+
 }
--- booking/src/test/java/com/komsije/booking/e2eTests/tests/SearchAndFilterTest.java
+++ booking/src/test/java/com/komsije/booking/e2eTests/tests/SearchAndFilterTest.java
@@ -1,5 +1,15 @@
 package com.komsije.booking.e2eTests.tests;
 
-public class SearchAndFilterTest {
+import com.komsije.booking.e2eTests.pages.HomePage;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SearchAndFilterTest extends TestBase{
+    @Test
+    public void test(){
+        HomePage homePage = new HomePage(driver);
+        assertTrue(homePage.isOpen());
+    }
 
 }

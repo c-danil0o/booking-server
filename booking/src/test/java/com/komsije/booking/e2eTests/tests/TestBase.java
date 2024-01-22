0cbcabf63ebae98cc7fdeaa9519a646f64df67f3
danilo_c <danilo.cvijetic10@protonmail.ch>
2024-01-22 14:17:09 +0100

    feat: added integration with spring test profile

--- booking/src/test/java/com/komsije/booking/e2eTests/tests/TestBase.java
+++ booking/src/test/java/com/komsije/booking/e2eTests/tests/TestBase.java
@@ -3,11 +3,20 @@ package com.komsije.booking.e2eTests.tests;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
-
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT, properties = "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.h2.H2ConsoleAutoConfiguration")
+@ActiveProfiles("test")
+@TestInstance(TestInstance.Lifecycle.PER_METHOD)
 public class TestBase {
     public static WebDriver driver;
 
@@ -34,5 +43,7 @@ public class TestBase {
     @Test
     public void test(){
         System.out.println("run");
+        WebDriverWait webDriverWait  = new WebDriverWait(driver, Duration.ofSeconds(30));
+        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("fda")));
     }
 }
